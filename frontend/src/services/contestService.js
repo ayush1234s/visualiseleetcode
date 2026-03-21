9e75060f168dbfdcf81b26d40a281fc3c5f2b096
Ayush Srivastava <ayushsrivastava8899@gmail.com>
2026-03-22 04:33:36 +0530

    Frontend Deploy

--- frontend/src/services/contestService.js
+++ frontend/src/services/contestService.js
@@ -1,8 +1,9 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchAllContests = async () => {
   try {
-    const res = await fetch(`${BASE_URL}/contests`);
+    const res = await fetch(`${BASE_URL}/api/contests`);
+    if (!res.ok) throw new Error("Contest fetch failed");
     return await res.json();
   } catch (err) {
     console.error("Contest Fetch Error:", err);
