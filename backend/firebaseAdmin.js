a7d9f4f307722be5759a4f044627bae7293eb882
Ayush Srivastava <ayushsrivastava8899@gmail.com>
2026-03-21 22:44:37 +0530

    Backend Deploy

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,13 +1,27 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
+let serviceAccount;
+
+if (process.env.FIREBASE_PRIVATE_KEY) {
+
+  // 🔐 Production (Render)
+  serviceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
+  };
+
+} else {
+
+  // 💻 Local (JSON file)
+  serviceAccount = require("./serviceAccountKey.json");
+
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 
-module.exports = { db };
+module.exports = { admin, db };
