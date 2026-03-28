9e2dbcef7b75efb684c8342bc2fb204a60a564b7
Ayush Srivastava <ayushsrivastava8899@gmail.com>
2026-03-28 23:26:11 +0530

    Backend Deploy

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -3,6 +3,7 @@ const { sendRevisionEmail } = require("../controllers/emailController");
 
 const router = express.Router();
 
+// This route is for revision reminder email only.
 router.post("/send-revision-email", sendRevisionEmail);
 
 module.exports = router;
